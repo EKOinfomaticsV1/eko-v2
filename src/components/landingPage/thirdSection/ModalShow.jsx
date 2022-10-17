8e934699db18dc19cdfe71cdcddea0ab7821dc90
Daniel-Magar <daniel.t@ekoinfomatics.com>
2022-10-17 15:33:20 +0530

    third section updated with modals

--- src/components/landingPage/thirdSection/ModalShow.jsx
+++ src/components/landingPage/thirdSection/ModalShow.jsx
@@ -1,68 +1,135 @@
-import React from 'react';
-import 'react-responsive-modal/styles.css';
-import { Modal } from 'react-responsive-modal';
-import RetailModal from '../../../assets/third_section/Modal/RetailModal.png'
+import React from "react";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
 
-
-
-const ModalShow = ({onOpenModal, onCloseModal, open, setOpen}) => {
-  
-
-    return (
-      <>
-       <div className='bg-black'>
-    
-      <Modal open={open} onClose={onCloseModal} center classNames={{
-          overlay: 'customOverlay',
-          modal: 'customModal',
-        }}>
-       <div className='bg-[#061137] border-[#0074E5] border border-opacity-50 p-5 rounded'>
-        <div className='flex flex-col justify-center items-center w-full'>
-            <div>
-                <h1 className='text-[48px]'>RETAIL</h1>
+const ModalShow = ({
+  onOpenModal,
+  onCloseModal,
+  open,
+  setOpen,
+  data,
+  image,
+}) => {
+  const closeIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      // style=" fill:#000000;"
+      fill="#000000"
+    >
+      <path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"></path>
+    </svg>
+    // <svg
+    //   fill="currentColor"
+    //   viewBox="0 0 20 20"
+    //   width={28}
+    //   height={28}
+    //   className="m-5"
+    // >
+    //   <path
+    //     fillRule="evenodd"
+    //     d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+    //     clipRule="evenodd"
+    //   ></path>
+    // </svg>
+  );
+  return (
+    <>
+      {/* <div className="bg-black"> */}
+      <div className="">
+        <Modal
+          open={open}
+          onClose={onCloseModal}
+          hideModalContentWhileAnimating={true}
+          center
+          classNames={{
+            overlay: "customOverlay",
+            modal: "customModal",
+          }}
+          // closeIcon={closeIcon}
+          showCloseIcon={false}
+        >
+          <div className="bg-[#061137] border-[#0074E5] border border-opacity-50 bg-opacity-90 p-10 rounded">
+            <div className="flex flex-col justify-center items-center w-full">
+              <div className="py-10">
+                <h1 className="text-[48px]">{data.name}</h1>
+              </div>
+              <div>
+                <img
+                  src={image}
+                  alt="Retail"
+                  className="w-sm md:w-md lg:w-[20rem]"
+                />
+              </div>
+              <div className="flex flex-col justify-center items-center w-[85%]">
+                <h1 className="py-8 font-semibold">Overview</h1>
+                <p className="text-center">{data.data1}</p>
+                <p className="text-center py-5">{data.data2}</p>
+                {data.data3 ? (
+                  <div className="flex flex-col justify-center items-center">
+                    <div className="flex justify-center items-center gap-2">
+                      <div>
+                        <div className="w-[0.5rem] h-[0.5rem] bg-white rounded"></div>
+                      </div>
+                      <div>{data?.data3?.l1}</div>
+                    </div>
+                    <div className="flex justify-center items-center gap-2">
+                      <div className="w-[0.5rem] h-[0.5rem] bg-white rounded"></div>
+                      <div>{data?.data3?.l2}</div>
+                    </div>
+                    <div className="flex justify-center items-center gap-2">
+                      <div className="w-[0.5rem] h-[0.5rem] bg-white rounded"></div>
+                      <div>{data?.data3?.l3}</div>
+                    </div>
+                    {data?.data3?.l4 ? (
+                      <>
+                        <div className="flex justify-center items-center gap-2">
+                          <div className="w-[0.5rem] h-[0.5rem] bg-white rounded"></div>
+                          <div>{data?.data3?.l4}</div>
+                        </div>
+                        <div className="flex justify-center items-center gap-2">
+                          <div className="w-[0.5rem] h-[0.5rem] bg-white rounded"></div>
+                          <div>{data?.data3?.l5}</div>
+                        </div>
+                        <div className="flex justify-center items-center gap-2">
+                          <div className="w-[0.5rem] h-[0.5rem] bg-white rounded"></div>
+                          <div>{data?.data3?.l6}</div>
+                        </div>
+                        <div className="flex justify-center items-center gap-2">
+                          <div className="w-[0.5rem] h-[0.5rem] bg-white rounded"></div>
+                          <div>{data?.data3?.l7}</div>
+                        </div>
+                        <div className="flex justify-center items-center gap-2">
+                          <div className="w-[0.5rem] h-[0.5rem] bg-white rounded"></div>
+                          <div>{data?.data3?.l8}</div>
+                        </div>
+                        <div className="flex justify-center items-center gap-2">
+                          <div className="w-[0.5rem] h-[0.5rem] bg-white rounded"></div>
+                          <div>{data?.data3?.l9}</div>
+                        </div>
+                        <div className="flex justify-center items-center gap-2">
+                          <div className="w-[0.5rem] h-[0.5rem] bg-white rounded"></div>
+                          <div>{data?.data3?.l10}</div>
+                        </div>
+                      </>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
             </div>
-             <div>
-                <img src={RetailModal} alt="Retail"  className='w-sm md:w-md lg:w-[20rem]'/>
-             </div>
-             <div className='flex flex-col justify-center items-center w-[85%]'>
-                <h1>Overview</h1>
-             <p className='text-center'>
-             Retail is the process of selling consumer goods or services to customers through multiple channels of distribution to earn a profit. Retailers satisfy demand identified through a supply chain. The term "retailer" is typically applied where a service provider fills the small orders of a large number of individuals, who are end-users, rather than large orders of a small number of wholesale, corporate or government clientele.
+          </div>
+        </Modal>
+      </div>
+    </>
+  );
+};
 
-           
-             </p>
-             <p className='text-center py-5'>
-             Shopping generally refers to the act of buying products. Sometimes this is done to obtain final goods, including necessities such as food and clothing; sometimes it takes place as a recreational activity. Recreational shopping often involves window shopping and browsing: it does not always result in a purchase.
-      
-             </p>
-             <div className='flex flex-col justify-center items-center'>
-               <div className='flex justify-center items-center gap-2'>
-                <div>
-                    <div className='w-[0.5rem] h-[0.5rem] bg-white rounded'></div>
-                </div>
-                <div>Supply Chain Analytics</div>
-               </div>
-               <div className='flex justify-center items-center gap-2'>
-               <div className='w-[0.5rem] h-[0.5rem] bg-white rounded'></div>
-                <div>Operational analytics</div>
-               </div>
-               <div className='flex justify-center items-center gap-2'>
-               <div className='w-[0.5rem] h-[0.5rem] bg-white rounded'></div>
-                <div>Customer Analytics</div>
-               </div>
-             </div>
-
-
-             </div>
-        </div>
-       </div>
-       
-   
-       
-      </Modal>
-    </div>
-      </>
-    );
-  };
-  
-  export default ModalShow;
+export default ModalShow;
--- src/components/landingPage/thirdSection/ThirdSection.jsx
+++ src/components/landingPage/thirdSection/ThirdSection.jsx
@@ -1,29 +1,108 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 // assets
 import thridSectionImg from "../../../assets/third_section/thridSectionImg.svg";
 import earth from "../../../assets/third_section/earth.mp4";
 import retail from "../../../assets/third_section/retail.png";
 import Retail from "../../../assets/third_section/Retail.png";
+
 import Defence from "../../../assets/third_section/Defence.png";
+
 import Manufacture from "../../../assets/third_section/Manufacture.png";
 import Banking from "../../../assets/third_section/Banking.png";
 import Healthcare from "../../../assets/third_section/Healthcare.png";
-import 'react-responsive-modal/styles.css';
-import { Modal } from 'react-responsive-modal';
+
+import RetailModal from "../../../assets/third_section/Modal/RetailModal.png";
+import TelecomModal from "../../../assets/third_section/Modal/TelecomModal.png";
+import EducationModal from "../../../assets/third_section/Modal/EducationModal.png";
+import BankingModal from "../../../assets/third_section/Modal/BankingModal.png";
+
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
 import ModalShow from "./ModalShow";
+
 function ThirdSection() {
+  // const modals = ["Retail", "Manufacture", "Defence", "Banking"];
   const [open, setOpen] = useState(false);
+  const [data, setData] = useState({});
+  const [image, setImage] = useState();
 
   const onOpenModal = () => setOpen(true);
   const onCloseModal = () => setOpen(false);
-  
+
+  function openModals(m) {
+    if (m === "retail") {
+      setOpen(true);
+      setImage(RetailModal);
+      setData({
+        name: "RETAIL",
+        data1:
+          " Retail is the process of selling consumer goods or services to customers through multiple channels of distribution to earn a profit. Retailers satisfy demand identified through a supply chain. The term retailer is typically applied where a service provider fills the small orders of a large number of individuals, who are end-users, rather than large orders of a small number of wholesale, corporate or government clientele.",
+        data2:
+          "Shopping generally refers to the act of buying products.Sometimes this is done to obtain final goods, including necessities such as food and clothing; sometimes it takes place as a recreational activity. Recreational shopping often involves window shopping and browsing: it does not always result in a purchase..",
+        data3: {
+          l1: "Supply Chain Analytics",
+          l2: "Operational analytics",
+          l3: "Customer Analytics",
+        },
+      });
+    } else if (m === "telecom") {
+      setOpen(true);
+      setImage(TelecomModal);
+      setData({
+        name: "TELECOM",
+        data1:
+          " Telecommunication is the transmission of signs, signals, messages, words, writings, images and sounds or information of any nature by wire, radio, optical or electromagnetic systems. Telecommunication occurs when the exchange of information between communication participants includes the use of technology. It is transmitted either electrically over physical media, such as cables, or via electromagnetic radiation Such transmission paths are often divided into communication channels which afford the advantages of multiplexing.",
+        data2:
+          "Since the Latin term communicatio is considered the social process of information exchange, the term telecommunications is often used in its plural form because it involves many different technologies.",
+      });
+    } else if (m === "education") {
+      setOpen(true);
+      setImage(EducationModal);
+      setData({
+        name: "EDUCATION",
+        data1:
+          "Digital Educational Platform which supports online Transactions, Education and go digital with minimal loss in time and resources Integrate core business functions Easy-to-configure Centrally Managed Solution (Managed by EKO) Streamline processes Plug and Play and Custom Enhancement / Development",
+
+        data2:
+          "Since the Latin term communicatio is considered the social process of information exchange, the term telecommunications is often used in its plural form because it involves many different technologies.",
+      });
+    } else if (m === "banking") {
+      setOpen(true);
+      setImage(BankingModal);
+      setData({
+        name: "BANKING",
+        data1:
+          "The world of banking has encountered unprecedented change over the past few years. Today, banks have that rare opportunity to reinvent themselves with data and analytics. Analytics will be a differentiator using analytics to gain an edge in a cutthroat environment—by improving risk assessment and predicting customer behavior. Analytics should be a part of every major initiative, in areas ranging from customers and risk to finance, workforce, and supply chain.",
+
+        data2:
+          "Since the Latin term communicatio is considered the social process of information exchange, the term telecommunications is often used in its plural form because it involves many different technologies.",
+        data3: {
+          l1: "Analytics can help",
+          l2: "Increase the ability to address and monitor regulatory compliance",
+          l3: "Increase transparency and understanding of risk exposures to manage the business more effectively",
+          l4: "Develop a risk-adjusted view of performance",
+          l5: "Manage fraud effectively",
+          l6: "Measure customer and product profitability",
+          l7: "Identify “high-potential” prospects and customers",
+          l8: "Improve the ability to target products and services to prospects or customers",
+          l9: "Enhance specific elements of the offer—product, pricing, channel",
+          l10: "Allow senior management to make informed operational decisions",
+        },
+      });
+    } else {
+      setOpen(flase);
+      setData({});
+    }
+  }
+
   return (
     <section className="bg-black py-20 text-white overflow-hidden">
-      <div className=" text-right text-3xl lg:text-4xl  text-white px-5 mb-20 font-semibold tracking-widest  ">
+      <div className=" text-right text-3xl  text-white px-5 mb-14 md:mb-20 font-semibold tracking-widest md:text-3xl  lg:text-7xl ">
         OUR SOLUTIONS
       </div>
-      <div className="flex h-full border">
-        <div className="flex justify-between items-center w-fit sm:w-full  border border-red-500">
+      <div className="flex h-full ">
+        {/* <div className="flex justify-between items-center w-fit sm:w-full  border border-red-500"> */}
+        <div className="flex justify-between items-center w-fit sm:w-full  ">
           <div className="  flex-1 md:flex-[0.4] lg:flex-[0.35] xl:flex-[0.3] 2xl:flex-[0.25] 2xl:max-w-[30vw] ">
             <video
               autoPlay
@@ -43,21 +122,17 @@ function ThirdSection() {
 
           <div className=" flex-1 md:flex-[0.6] lg:flex-[0.65] xl:flex-[0.7] 2xl:flex-[0.75] flex flex-col gap-5 items-center   ml-[-5rem] md:ml-[-2rem] lg:ml-[3rem] xl:ml-[4rem] 2xl:ml-[1rem]">
             {/* 1 */}
-            {/* <div className="w-full flex items-center z-10 translate-x-[5%] sm:-translate-x-[10%] translate-y-[5%] overflow-hidden">
-              <div className="flex items-start w-[30%] sm:w-[50%] md:w-[40%] lg:w-[22%] xl:w-[18%]">
-                <div className="bg-[#0085FE] aspect-square w-[8px] rounded-full -translate-y-[2.5px]"></div>
-                <div className="border-t-[#0085FE] border-t-2 border-dashed flex-1"></div>
-              </div>
 
-              <div className="rounded-lg p-2 border-[#0085FE] aspect-square w-[65%]  max-w-[75px] sm:max-w-[100px] md:max-w-[100px] lg:max-w-[130px] xl:max-w-[150px] bg-white"></div>
-            </div> */}
-            <div className="w-full flex items-center z-10 translate-x-[5%] sm:-translate-x-[10%] translate-y-[10%] sm:translate-y-[10%] md:-translate-x-[5%] lg:-translate-x-[10%] xl:-translate-x-[15%] 2xl:-translate-x-[5%]  lg:translate-y-[40%] xl:translate-y-[40%] 2xl:translate-y-[40%]   overflow-hidden">
+            <div className="w-full flex items-center z-10 translate-x-[10%] sm:-translate-x-[2%] translate-y-[10%] sm:translate-y-[10%] md:translate-x-[4%] lg:-translate-x-[10%] xl:-translate-x-[15%] 2xl:-translate-x-[5%]  lg:translate-y-[15%] xl:translate-y-[0%] 2xl:translate-y-[0%]   overflow-hidden">
               <div className="flex items-start w-[28%] sm:w-[50%] md:w-[40%] lg:w-[22%] xl:w-[18%]">
                 <div className="bg-[#0085FE] aspect-square w-[8px] rounded-full -translate-y-[2.5px]"></div>
                 <div className="border-t-[#0085FE] border-t-2 border-dashed flex-1"></div>
               </div>
 
-              <div onClick={() => setOpen(true)} className="cursor-pointer rounded-lg p-2 aspect-square w-[65%] max-w-[75px] sm:max-w-[100px] md:max-w-[100px] lg:max-w-[100px] xl:max-w-[120px]  2xl:max-w-[150px] bg-[#061137] border-[#0074E5] border border-opacity-50">
+              <div
+                onClick={() => openModals("retail")}
+                className="cursor-pointer rounded-lg p-2 aspect-square w-[65%] max-w-[75px] sm:max-w-[100px] md:max-w-[100px] lg:max-w-[100px] xl:max-w-[120px]  2xl:max-w-[150px] bg-[#061137] border-[#0074E5] border border-opacity-50"
+              >
                 <h1 className="text-white font-semibold text-sm sm:text-base md:text-sm lg:text-lg xl:text-xl xl:w-[80%] xl:mx-auto   text-center  mb-2 lg:mb-5 ">
                   RETAIL
                 </h1>
@@ -68,15 +143,18 @@ function ThirdSection() {
             </div>
 
             {/* 2 */}
-            <div className="w-full flex items-center z-10 translate-x-[25%] sm:translate-x-[15%] md:translate-x-[10%] lg:translate-x-[5%] translate-y-[10%] lg:translate-y-[12%] xl:translate-y-[10%] overflow-hidden">
+            <div className="w-full flex items-center z-10 translate-x-[25%] sm:translate-x-[15%] md:translate-x-[16%] lg:translate-x-[5%] translate-y-[10%] lg:translate-y-[12%] xl:translate-y-[10%] overflow-hidden">
               <div className="flex items-start w-[28%] sm:w-[50%] md:w-[40%] lg:w-[30%] xl:w-[25%]">
                 <div className="bg-[#0085FE] aspect-square w-[8px] rounded-full -translate-y-[2.5px]"></div>
                 <div className="border-t-[#0085FE] border-t-2 border-dashed flex-1"></div>
               </div>
 
-              <div className="rounded-lg p-2 aspect-square w-[65%] max-w-[75px] sm:max-w-[100px] md:max-w-[100px] lg:max-w-[100px] xl:max-w-[120px]  2xl:max-w-[150px] bg-[#061137] border-[#0074E5] border border-opacity-50">
+              <div
+                onClick={() => openModals("telecom")}
+                className="cursor-pointer rounded-lg p-2 aspect-square w-[65%] max-w-[75px] sm:max-w-[100px] md:max-w-[100px] lg:max-w-[100px] xl:max-w-[120px]  2xl:max-w-[150px] bg-[#061137] border-[#0074E5] border border-opacity-50"
+              >
                 <h1 className="text-white font-semibold text-[12px] sm:text-base md:text-sm lg:text-md xl:text-lg  xl:mx-auto   text-center  mb-2 lg:mb-5 ">
-                  MANUFACTURE
+                  TELECOM
                 </h1>
                 <div className="aspect-square  mx-auto">
                   <img src={Manufacture} alt="telecom" />
@@ -84,21 +162,21 @@ function ThirdSection() {
               </div>
             </div>
             {/* 3 */}
-            <div className="w-full flex items-center z-10 translate-x-[30%] sm:translate-x-[15%] md:translate-x-[16%] lg:translate-x-[5%]  translate-y-[15%] xl:translate-x-[5%] xl:translate-y-[5%] overflow-hidden">
+            {/* <div className="w-full flex items-center z-10 translate-x-[30%] sm:translate-x-[15%] md:translate-x-[16%] lg:translate-x-[5%]  translate-y-[15%] xl:translate-x-[5%] xl:translate-y-[5%] overflow-hidden">
               <div className="flex items-start w-[10%] sm:w-[30%] md:w-[10%] lg:w-[20%] xl:w-[10%]">
                 <div className="bg-[#0085FE] aspect-square w-[8px] rounded-full -translate-y-[2.5px]"></div>
                 <div className="border-t-[#0085FE] border-t-2 border-dashed flex-1"></div>
               </div>
 
               <div className="rounded-lg p-2 aspect-square w-[65%] max-w-[75px] sm:max-w-[100px] md:max-w-[100px] lg:max-w-[100px] xl:max-w-[120px]  2xl:max-w-[150px] bg-[#061137] border-[#0074E5] border border-opacity-50">
-              <h1 className="text-white font-semibold text-[12px] sm:text-base md:text-sm lg:text-md xl:text-lg  xl:mx-auto   text-center  mb-2 lg:mb-5 ">
+                <h1 className="text-white font-semibold text-[12px] sm:text-base md:text-sm lg:text-md xl:text-lg  xl:mx-auto   text-center  mb-2 lg:mb-5 ">
                   HEALTHCARE
                 </h1>
                 <div className="aspect-square  mx-auto">
                   <img src={Healthcare} alt="Healthcare" />
                 </div>
               </div>
-            </div>
+            </div> */}
             {/* 4 */}
             <div className="w-full flex items-center z-10  translate-y-[20%] translate-x-[25%] md:translate-x-[16%] lg:translate-x-[5%]  sm:translate-x-[15%] lg:translate-y-[5%] overflow-hidden">
               <div className="flex items-start w-[28%] sm:w-[50%] md:w-[40%] lg:w-[30%] xl:w-[25%]">
@@ -106,9 +184,12 @@ function ThirdSection() {
                 <div className="border-t-[#0085FE] border-t-2 border-dashed flex-1"></div>
               </div>
 
-              <div className="rounded-lg p-2 aspect-square w-[65%] max-w-[75px] sm:max-w-[100px] md:max-w-[100px] lg:max-w-[100px] xl:max-w-[120px]  2xl:max-w-[150px] bg-[#061137] border-[#0074E5] border border-opacity-50">
-                <h1 className="text-white font-semibold text-sm sm:text-base md:text-sm lg:text-lg xl:text-xl xl:w-[80%] xl:mx-auto  tracking-[0.2em] text-center  mb-2 lg:mb-5 ">
-                  DEFENCE
+              <div
+                onClick={() => openModals("education")}
+                className="cursor-pointer rounded-lg p-2 aspect-square w-[65%] max-w-[75px] sm:max-w-[100px] md:max-w-[100px] lg:max-w-[100px] xl:max-w-[120px]  2xl:max-w-[150px] bg-[#061137] border-[#0074E5] border border-opacity-50"
+              >
+                <h1 className="text-white font-semibold text-[12px] sm:text-base md:text-sm lg:text-md xl:text-lg  xl:mx-auto   text-center  mb-2 lg:mb-5 ">
+                  EDUCATION
                 </h1>
                 <div className="aspect-square  mx-auto">
                   <img src={Defence} alt="Defence" />
@@ -116,34 +197,37 @@ function ThirdSection() {
               </div>
             </div>
             {/* 5 */}
-            {/* <div className="w-full flex items-center translate-x-[5%] sm:-translate-x-[10%]  translate-y-[20%] overflow-hidden">
-              <div className="flex items-start w-[20%] sm:w-[50%] ">
-                <div className="bg-[#0085FE] aspect-square w-[8px] rounded-full -translate-y-[2.5px]"></div>
-                <div className="border-t-[#0085FE] border-t-2 border-dashed flex-1"></div>
-              </div>
 
-              <div className="rounded-lg p-2 border-[#0085FE] aspect-square w-[65%] bg-white max-w-[75px] sm:max-w-[100px] "></div>
-            </div> */}
-            <div className="w-full flex items-center z-10 translate-x-[5%] sm:-translate-x-[10%] translate-y-[20%] lg:-translate-x-[10%] xl:-translate-x-[18%] 2xl:-translate-x-[6%]    lg:-translate-y-[24%]   overflow-hidden">
+            <div className="w-full flex items-center z-10 translate-x-[10%] sm:-translate-x-[2%] translate-y-[10%] sm:translate-y-[10%] md:translate-x-[4%] lg:-translate-x-[10%] xl:-translate-x-[15%] 2xl:-translate-x-[5%]  lg:-translate-y-[8%] xl:translate-y-[0%] 2xl:translate-y-[0%]   overflow-hidden">
               <div className="flex items-start w-[28%] sm:w-[50%] md:w-[40%] lg:w-[22%] xl:w-[18%]">
                 <div className="bg-[#0085FE] aspect-square w-[8px] rounded-full -translate-y-[2.5px]"></div>
                 <div className="border-t-[#0085FE] border-t-2 border-dashed flex-1"></div>
               </div>
 
-              <div className="rounded-lg p-2 aspect-square w-[65%] max-w-[75px] sm:max-w-[100px] md:max-w-[100px] lg:max-w-[100px] xl:max-w-[120px]  2xl:max-w-[150px] bg-[#061137] border-[#0074E5] border border-opacity-50">
-                <h1 className="text-white font-semibold text-sm sm:text-base md:text-sm lg:text-lg xl:text-xl xl:w-[80%] xl:mx-auto  tracking-[0.2em] text-center  mb-2 lg:mb-5 ">
+              <div
+                onClick={() => openModals("banking")}
+                className="cursor-pointer rounded-lg p-2 aspect-square w-[65%] max-w-[75px] sm:max-w-[100px] md:max-w-[100px] lg:max-w-[100px] xl:max-w-[120px]  2xl:max-w-[150px] bg-[#061137] border-[#0074E5] border border-opacity-50"
+              >
+                <h1 className="text-white font-semibold text-sm sm:text-base md:text-sm lg:text-lg xl:text-xl xl:w-[80%] xl:mx-auto   text-center  mb-2 lg:mb-5 ">
                   BANKING
                 </h1>
                 <div className="aspect-square  mx-auto">
-                  <img src={Banking} alt="retail" />
+                  <img src={BankingModal} alt="banking" />
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <ModalShow onOpenModal={onOpenModal} onCloseModal={onCloseModal} open={open} setOpen={setOpen}/>
+
+        <ModalShow
+          data={data}
+          open={open}
+          setOpen={setOpen}
+          onCloseModal={onCloseModal}
+          image={image}
+        />
         <div className=" flex flex-col  w-fit  pr-5 ">
-          <h1 className="vertival-rl tracking-[1.2em] text-xl  flex items-end justify-start   flex-1">
+          <h1 className="vertival-rl tracking-[1.2em] sm:tracking-[1.7em] text-xl lg:text-2xl xl:text-3xl flex items-end justify-start   flex-[0.7]">
             POWERS
           </h1>
 
@@ -151,11 +235,10 @@ function ThirdSection() {
         </div>
       </div>
 
-      <h1 className=" text-right text-3xl lg:text-4xl  text-white px-5 mt-20  w-min ml-auto font-semibold tracking-widest z-10">
+      <h1 className=" text-right text-3xl md:text-3xl  lg:text-7xl  text-white px-5 mt-20  w-min ml-auto  tracking-widest z-10">
         MULTILE INDUSTRIES
       </h1>
     </section>
-    
   );
 }
 
